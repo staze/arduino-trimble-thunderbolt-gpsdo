1e9b07e6164402630240a20770b7ed10007ab2de
andrew@n7ul.com <andrew@n7ul.com@f50d28bd-be5d-d1ba-b5b2-2aa2a810ebf5>
2011-04-08 07:00:16 +0000

    Changed array indexing to post increment. Fixes off by one bug due to array indexing. m_num_chars now reflects number of bytes in m_read_buffer. Fixed up syntax issues and removed crufty fragment.

--- trunk/libraries/Trimble/Trimble.cpp
+++ trunk/libraries/Trimble/Trimble.cpp
@@ -5,56 +5,56 @@
 
 Trimble::Trimble()
 {
-    m_num_chars = -1;
+   m_num_chars = 0;
 	m_state = START;
 }
  
 bool Trimble::encode(byte c)
 {
-	switch(m_state) {
-	case START:
-		if(m_state == START)
-		if(c == DLE)
-			m_state = FRAMING_DLE;
-		break;
-	case FRAMING_DLE:
-		//mis-framed
-		if(c == DLE || c == ETX)
-			m_state = START:
-		else
-		{
-			state = DATA;
-			m_num_chars = 0;
-			m_read_buffer[++m_num_chars] = c;
-		}
-		break;
+  switch (m_state)
+  {
+    case START:
+      if (c == DLE)
+        m_state = FRAMING_DLE;
+     break;
+   case FRAMING_DLE:
+	  //mis-framed
+	  if (c == DLE || c == ETX)
+		 m_state = START;
+	  else
+	  {
+		 state = DATA;
+		 m_num_chars = 0;
+		 m_read_buffer[m_num_chars++] = c;
+	  }
+	  break;
 	case DATA:
-		//found data DLS
-		if(c == DLE)
-			m_state = DATA_DLE;
-		//add byte to packet
-		else
-			m_read_buffer[++m_num_chars] = c;
-		break;
+	  //found data DLE
+	  if (c == DLE)
+		 m_state = DATA_DLE;
+	  //add byte to packet
+	  else
+		 m_read_buffer[m_num_chars++] = c;
+	  break;
 	case DATA_DLE:
-		if(c == DLE)
-		{
-			m_read_buffer[++m_num_chars] = c;
-			m_state = DATA;
-		}
-		else if(c == ETX)
-		{
-			m_state = START;
-			parsePacket();   //Whoohoo the moment we've been waitin for
-			return TRUE;
-		}
-		//mis-framed
-		else
-			m_state = START;
-		break;
+	  if (c == DLE)
+	  {
+		 m_read_buffer[m_num_chars++] = c;
+		 m_state = DATA;
+	  }
+	  else if (c == ETX)
+	  {
+		 m_state = START;
+		 parsePacket();   //Whoohoo the moment we've been waitin for
+		 return TRUE;
+	  }
+	  //mis-framed
+	  else
+		 m_state = START;
+	  break;
 	default:
-		m_state = START;
-		break;
+	  m_state = START;
+	  break;
 	}
 	return FALSE;
 }
