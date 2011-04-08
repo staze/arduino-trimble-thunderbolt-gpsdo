37c26a00545c949fa2b3c7e10d98a5a77f3fb884
andrew@n7ul.com <andrew@n7ul.com@f50d28bd-be5d-d1ba-b5b2-2aa2a810ebf5>
2011-04-08 07:06:41 +0000

    Fixed up uneven indentation due to online editor.

--- trunk/libraries/Trimble/Trimble.cpp
+++ trunk/libraries/Trimble/Trimble.cpp
@@ -16,45 +16,45 @@ bool Trimble::encode(byte c)
     case START:
       if (c == DLE)
         m_state = FRAMING_DLE;
-     break;
-   case FRAMING_DLE:
-	  //mis-framed
-	  if (c == DLE || c == ETX)
-		 m_state = START;
-	  else
-	  {
-		 state = DATA;
-		 m_num_chars = 0;
-		 m_read_buffer[m_num_chars++] = c;
-	  }
-	  break;
-	case DATA:
-	  //found data DLE
-	  if (c == DLE)
-		 m_state = DATA_DLE;
-	  //add byte to packet
-	  else
-		 m_read_buffer[m_num_chars++] = c;
-	  break;
-	case DATA_DLE:
-	  if (c == DLE)
-	  {
-		 m_read_buffer[m_num_chars++] = c;
-		 m_state = DATA;
-	  }
-	  else if (c == ETX)
-	  {
-		 m_state = START;
-		 parsePacket();   //Whoohoo the moment we've been waitin for
-		 return TRUE;
-	  }
-	  //mis-framed
-	  else
-		 m_state = START;
-	  break;
-	default:
-	  m_state = START;
-	  break;
-	}
-	return FALSE;
+      break;
+    case FRAMING_DLE:
+      //mis-framed
+      if (c == DLE || c == ETX)
+        m_state = START;
+      else
+      {
+        state = DATA;
+        m_num_chars = 0;
+        m_read_buffer[m_num_chars++] = c;
+      }
+      break;
+    case DATA:
+      //found data DLE
+      if (c == DLE)
+        m_state = DATA_DLE;
+      //add byte to packet
+      else
+        m_read_buffer[m_num_chars++] = c;
+      break;
+    case DATA_DLE:
+      if (c == DLE)
+      {
+        m_read_buffer[m_num_chars++] = c;
+        m_state = DATA;
+      }
+      else if (c == ETX)
+      {
+        m_state = START;
+        parsePacket();   //Whoohoo the moment we've been waitin for
+        return TRUE;
+      }
+      //mis-framed
+      else
+        m_state = START;
+      break;
+    default:
+      m_state = START;
+      break;
+  }
+  return FALSE;
 }
